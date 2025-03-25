760d01493e4c8e1c9d12ef24317a9855a2c88116
GustavoinCode24 <gugaprogramador24@gmail.com>
2025-03-25 00:14:43 -0300

    casa de aposta

---
+++ login.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casa de Aposta</title>
+</head>
+<body>
+
+        <form action=method = "POST" action = "#">
+            <label for="usuario">Digite o nome de usuario</label>
+            <input type="text" id="usuario" name="usuario" required>
+            <input type="submit" value="Entrar" name="btnUser">
+        </form>
+
+
+
+</body>
+</html>
