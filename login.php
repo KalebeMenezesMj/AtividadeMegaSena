9c23573e237666dcc82b5da5499992605278af83
KalebeMenezesMj <kalebe.menezesmj@gmail.com>
2025-03-31 13:48:00 -0300

    megasenasla

--- login.php
+++ login.php
@@ -1,44 +1,61 @@
+<?php
 
+if (isset($_POST["btnUser"])) {
+
+    session_start();
+
+    $_SESSION["usuario"] = $_POST["usuario"];
+    header("location: index.php");
+}
+
+if (isset($_COOKIE["email"])) {
+
+    $email = $_COOKIE["email"];
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
     <title>Casa de Aposta</title>
 </head>
+
 <body>
 
     <div id="borda">
-    <div class="form-usuario">
-        <form action="#" method="POST">
-            <label for="usuario">Digite um email</label><br>
-            <input type="text" id="usuario" name="usuario" required><br>
-    
-            <label for="senha">Digite sua senha</label><br>
-            <input type="password" id="senha" name="senha" required><br><br>
-               
-            <input type="submit" value="Sair" name="btnSair">
-            <input type="submit" value="Entrar" name="btnUser">
-        </form>
+        <div class="form-usuario">
+            <form action="#" method="POST">
+                <label for="usuario">Digite um email</label><br>
+                <input type="text" id="email" name="email" required><br>
+
+                <label for="senha">Digite sua senha</label><br>
+                <input type="password" id="senha" name="senha" required><br><br>
+
+                <input type="submit" value="Entrar" name="btnUser">
+            </form>
+        </div>
     </div>
-</div>
 
 
-        <?php 
+    <?php
 
-        if($_POST){
+    if ($_POST) {
 
-            $email = "aluno@fatec.edu.br";
-            $senha = "alunoweb2";
+        $email = "aluno@fatec.edu.br";
+        $senha = "alunoweb2";
 
-            $email = $_POST ['usuario'];
-            $senha = $_POST['senha'];
+        $email = $_POST['usuario'];
+        $senha = $_POST['senha'];
+    }
 
-        }
-            
-        ?>
+    ?>
 
 
 </body>
+
 </html>
